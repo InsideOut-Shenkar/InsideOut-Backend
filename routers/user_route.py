970b66d872fcb5d4f658c5312605e6978af4d139
Ibraheem Alnakib <ab2h3m@gmail.com>
2024-06-01 22:21:52 +0300

    Add POST route for user creation in user_blueprint.

--- routers/user_route.py
+++ routers/user_route.py
@@ -10,3 +10,7 @@ user_blueprint = Blueprint('user_blueprint', __name__)
 @user_blueprint.route('/<int:user_id>', methods=['GET'])
 def get_user_by_id(user_id):
     return user_controller.get_user_by_id(user_id)
+
+@user_blueprint.route('', methods=['POST'])
+def add_user():
+    return user_controller.add_user()
