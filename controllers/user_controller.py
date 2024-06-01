852f52d930e026f187c0cb62cce99de7d41d5b40
Ibraheem Alnakib <ab2h3m@gmail.com>
2024-06-01 22:21:11 +0300

    Enhance UserController with user repository integration and add user creation endpoint.

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,9 +1,24 @@
 # controllers/user_controller.py
 
+from flask import jsonify, request, json
+from repositories.user_repository import UserRepository
+
 class UserController:
-    @staticmethod
+    def __init__(self):
+        self.user_repository = UserRepository()
+
     def get_user_by_id(user_id):
         return {
             "userID": user_id
         }
+    
+    def add_user(self):
+        user_data = json.loads(request.data)
+        if not user_data:
+            return jsonify({'error': 'No user data provided'}), 400
+        try:
+            user_id = self.user_repository.add(user_data)
+            return jsonify({'message': 'Patient added successfully', 'id': user_id}), 201
+        except Exception as e:
+            return jsonify({'error': 'Failed to add patient', 'details': str(e)}), 500
 
