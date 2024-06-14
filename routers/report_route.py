dc585676d5ecefe2ca2116b212c2d9ed2515b049
Ibraheem Alnakib <ab2h3m@gmail.com>
2024-06-15 02:38:39 +0300

    Added report_route.py to handle report-related endpoints. Includes routes for fetching a report by ID and fetching all reports.

---
+++ routers/report_route.py
@@ -0,0 +1,16 @@
+# routers/report_route.py
+
+from flask import Blueprint 
+from controllers.report_controller import ReportController
+
+report_controller = ReportController()
+
+report_blueprint = Blueprint('report_blueprint', __name__)
+
+@report_blueprint.route('/<int:report_id>', methods=['GET'])
+def get_report_by_id(report_id):
+    return report_controller.get_report(report_id)
+
+@report_blueprint.route('', methods=['GET'])
+def get_reports():
+    return report_controller.get_reports()
