3501076e2062e6749dd484fd4e9a6e4e5f3e856f
Ibraheem Alnakib <ab2h3m@gmail.com>
2024-06-15 02:40:09 +0300

    Refactored `assessment_controller.py` to include handling for fetching common columns, initializing RiskAssessor, and processing risk assessment with report creation and model predictions storage.

--- controllers/assessment_controller.py
+++ controllers/assessment_controller.py
@@ -2,24 +2,56 @@
 
 from datetime import datetime
 from flask import jsonify, request
-from repositories.patient_repository import PatientRepository
 from repositories.user_repository import UserRepository
-from ml.prediction import calculate_risk
+from repositories.patient_repository import PatientRepository
+from repositories.medical_data_repository import MedicalDataRepository
+from repositories.report_repository import ReportRepository
+
+from ml.risk_assessor import RiskAssessor
+from ml.data_inputer import DataInputer
+from ml.s3_file_manager import file_manager
+
+columns = file_manager.fetch_json('common_columns.json')
+data_inputer = DataInputer(common_columns=columns)
+risk_assessor = RiskAssessor(data_inputer=data_inputer, common_columns=columns)
 
 class AssessmentController:
     def __init__(self):
-        self.patient_repository = PatientRepository()
         self.user_repository = UserRepository()
+        self.report_repository = ReportRepository()
+        self.patient_repository = PatientRepository()
+        self.medical_data_repository = MedicalDataRepository()
 
     def assess_risk(self):
         try:
-            patient_id, user_id, medical_info = self._validate_input()
-            patient = self._get_patient(patient_id)
-            user = self._get_user(user_id)[0]
-            if 'age' not in medical_info:
-                medical_info['age'] = self._calculate_age(patient['date_of_birth'])
-            risk_assessment = calculate_risk(medical_info)
-            return jsonify(risk_assessment), 200
+            patient_id, user_id, medical_data_id, weights = self._validate_input()
+            medical_data = self.medical_data_repository.get(medical_data_id)
+            # Construct the new object
+            medical_data = {item['name']: item['value'] for item in medical_data}
+            risk_level, assessment_score, model_predictions = risk_assessor.calculate_risk(
+                query=medical_data,
+                ds2_doctor_vote=int(weights['weight_1']),
+                ds4_doctor_vote=int(weights['weight_2'])
+            )
+            report_data = {
+                'patient_id': patient_id, 'created_by': user_id,
+                'medical_data_id': medical_data_id, 'risk_level': risk_level,
+                'assessment_score': assessment_score, 'ds2_vote': weights['weight_1'], 'ds4_vote': weights['weight_2']
+            }
+
+            report_id = self.report_repository.add(report_data)
+
+            for model_prediction in model_predictions:
+                model_prediction_data = {
+                    'model_name': model_prediction['name'], 'prediction': model_prediction['prediction'], 'model_accuracy': model_prediction['accuracy']
+                }
+                model_prediction_id = self.report_repository.add_model_prediction(model_prediction_data)
+                report_model_prediction_data = {
+                    'report_id': report_id, 'prediction_id': model_prediction_id
+                }
+                self.report_repository.add_report_model_prediction(report_model_prediction_data)
+
+            return jsonify({'id': report_id}), 200
         except Exception as e:
             return jsonify({'error': 'An error occurred processing the request', 'details': str(e)}), 500
 
